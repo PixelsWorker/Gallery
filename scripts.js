618b661e7745a36d8667adddd7ebc119d1ca5fdd
Rockson Kathar <151949464+PixelsWorker@users.noreply.github.com>
2025-03-28 11:10:21 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,18 @@
-// Function to open the clicked image in a modal
 function openImage(imageSrc) {
     var modal = document.getElementById('modal');
     var modalImg = document.getElementById('modalImg');
     
-    // Set the image source for the modal
     modalImg.src = imageSrc;
     
-    // Display the modal
     modal.style.display = 'block';
 }
 
-// Function to close the modal
 function closeImage() {
     var modal = document.getElementById('modal');
     
-    // Hide the modal
     modal.style.display = 'none';
 }
 
-// Close modal if clicking outside the image area
 window.onclick = function(event) {
     var modal = document.getElementById('modal');
     
